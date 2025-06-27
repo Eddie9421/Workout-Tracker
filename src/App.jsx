6a91847f6e1d06867595ecea3e5a479297fd0d38
Eddie <eddie11910@outlook.com>
2025-06-26 22:31:55 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import WorkoutList from "./components/WorkoutList.jsx";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <h1 className="text-3xl font-bold mb-4 text-center">Workout Tracker</h1>
+      <h1 className="text-3xl font-bold underline"> Hello world! </h1>
+      <WorkoutList />
+    </div>
+  );
+}
