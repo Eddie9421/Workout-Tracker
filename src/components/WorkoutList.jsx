90eaa985ea3a37046b4ecd96eba060c0efe08d86
Eddie <eddie11910@outlook.com>
2025-06-27 12:17:42 -0700

    Added workout creation button and started a function to add workouts

--- src/components/WorkoutList.jsx
+++ src/components/WorkoutList.jsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 import Workout from "./Workout.jsx";
 
 export default function WorkoutList() {
+  function addWorkout() {}
   const [workouts, setWorkouts] = useState([]);
 
   return (
     <>
       <h2 className="flex justify-center">Workouts</h2>
+      <button>Add Workouts</button>
     </>
   );
 }
