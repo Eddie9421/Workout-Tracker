3783e5115f44ae21a99d965e9171b8f51a36a210
Eddie <eddie11910@outlook.com>
2025-06-27 11:50:35 -0700

    Gave a heading to the workouts list

--- src/components/WorkoutList.jsx
+++ src/components/WorkoutList.jsx
@@ -4,4 +4,10 @@ import Workout from "./Workout.jsx";
 
 export default function WorkoutList() {
   const [workouts, setWorkouts] = useState([]);
+
+  return (
+    <>
+      <h2 className="flex justify-center">Workouts</h2>
+    </>
+  );
 }
